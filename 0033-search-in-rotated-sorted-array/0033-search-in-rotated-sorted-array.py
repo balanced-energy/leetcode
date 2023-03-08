a861d5c0ae2e9ecb930f055d6bdf46b182d1c536
Camden C <49416039+balanced-energy@users.noreply.github.com>
2023-03-08 14:01:38 -0700

    Time: 47 ms (38.03%), Space: 14.2 MB (46.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,41 +1,84 @@
 
 '''
-l      m     r
+Workflow Timestamps
+1. 0:00 - 2:15 Make Sure You Understand the Problem
+2. 2:15 - 8:45 Design a Solution / Runtime and Space Complexity
+3. 8:45 - 14: 45 Write a Template for Code in Logical Blocks. Aka Pseudocode
+4. Write the Code And Pass Test Cases.
+'''
+'''
+1. Make Sure You Understand the Problem
 
-[4,5,6,1,2,3] target = 2
+ l     m     r
+         l m r
+[4,5,6,7,0,1,2] target 0
 
-l      m     r
-[4,5,6,1,2,3] target = 5
+ l     m     r
+ l m r     
+[4,5,6,7,0,1,2] target 6
 
-Comparing lo and mid elements we can check which half of the array is still sorted.
-If start < mid then the first half is still sorted and we check if target is in that range
-if start > mid then we know mid to end is sorted and check if target is in that range 
+2. Design a Solution / Runtime and Space Complexity
+Perform binary search. We want to compare mid to the start element to determine which half of the array is still sorted.
+Once we know which is still sorted we check if target is within that sorted half, if so move pointers to continue on that 
+half, otherwise move pointers to other half.
 
+3. Write a Template for Code in Logical Blocks. Aka Pseudocode
+start = 0
+end = length of array - 1
+
+while start <= end
+
+# Compare mid element to start 
+calculate mid 
+
+Check if mid == target 
+
+# First half still sorted
+If nums[mid] > nums[start]
+    if nums[start] <= target < nums[mid] 
+        right = mid - 1
+    else 
+        left = mid + 1
+# Second half still sorted
+if nums[mid] < nums[start]
+    if nums[mid] <= target < nums[start] 
+        left = mid + 1
+    else 
+        right = mid - 1
+        
+return - 1
+4. Write the Code And Pass Test Cases.
+
+[4,5,6,7,0,1,2] target 6
+
+[5,1,3] target 3
 '''
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start = 0
         end = len(nums) - 1
-
+        
         while start <= end:
             mid = (start + end) // 2
-
+            
             if nums[mid] == target:
-                return mid 
-
-            # First half is still sorted
-            if nums[mid] >= nums[start]:
-                # Check if target within range
+                return mid
+            
+            # First half is sorted
+            if nums[start] <= nums[mid]:
                 if nums[start] <= target < nums[mid]:
                     end = mid - 1
+                    
                 else:
                     start = mid + 1
-            # Second half is still sorted 
+                    
+            # Second half is sorted
             else:
                 if nums[mid] < target <= nums[end]:
                     start = mid + 1
+                    
                 else:
                     end = mid - 1
-    
-
-        return -1
+        
+        return - 1
+        
