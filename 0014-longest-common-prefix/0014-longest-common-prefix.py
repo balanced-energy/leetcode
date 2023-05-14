1ad117fe1e5e14091814d0a2f3f2133c7948d346
Camden C <49416039+balanced-energy@users.noreply.github.com>
2023-05-13 19:31:07 -0600

    Time: 52 ms (15.60%), Space: 16.3 MB (17.96%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -6,8 +6,7 @@ class Solution:
         base = strs[0]
         for i in range(len(base)):
             for word in strs[1:]:
-                if (i == len(word) or 
-                    word[i] != base[i]):
+                if (i == len(word) or word[i] != base[i]):
                     return base[0:i]
 
         return base
