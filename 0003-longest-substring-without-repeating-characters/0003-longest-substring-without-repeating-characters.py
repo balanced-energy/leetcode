90fd289218e3db25bde1c19579a66db2832c6b97
Camden C <49416039+balanced-energy@users.noreply.github.com>
2023-06-16 16:09:22 -0600

    Time: 72 ms (73.30%), Space: 16.3 MB (72.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,73 +1,68 @@
 '''
-Workflow Timestamps
-1. 0:00 - 2:45 Make Sure You Understand the Problem
-2. 2:45 - 8:30 Design a Solution / Runtime and Space Complexity
-3. 8:30 - 14:10 Write a Template for Code in Logical Blocks. Aka Pseudocode
-4. 14:10 - 22:45 Write the Code And Pass Test Cases.
-'''
-'''
-1. Make Sure You Understand the Problem
-s = [] -> 0
-s = ['a'] -> 1
+Make Sure You Understand the Problem
+Design a Solution / Runtime and Space Complexity
+Write a Template for Code in Logical Blocks. Aka Pseudocode
+Write the Code And Pass Test Cases.
 
+- Two pointer approach
+- While end pointer is less than length s - 1
+- Move end pointer forward 
+- compare current length to max
+- if char not in set
+    - add to set
+- if char is in set
+ - move start pointer forward until duplicate char
+     - remove letters if not equal to duplicate char
+     - once at index of duplicate char move one more index
 
-2. Design a Solution / Runtime and Space Complexity
-Start and end pointers
-map to store char counts
-
-Increment end until window is not valid, not valid meaning current char count > 1 in map
-    not valid window 
-        increment start pointer until cur_char count == 1 
-
-save length of window 
-increment end 
-
-3. Write a Template for Code in Logical Blocks. Aka Pseudocode
-start = 0
-end = 0
-max_length = 0
-freq_map = {}
-
-# Loop through each character
-while end < len(s):
-    increment char count in map
+Two pointers l,r
+while r < len(s) - 1
+    start = s[l]
+    end = s[r]
+    if end not in map 
+        end to map
+        increment r
     
-    while char count > 1:
-        subtrack char count
-        increment start until char count == 1
-        
-    compare current length to max and update
-    increment end
-    
-return max_length
-4. Write the Code And Pass Test Cases.
+    else
+        increment l until (s[l] == s[r])
+        if s[l] not equalto s[r] 
+        remove sfrom map
+        then move l + 1 index 
+        compare r - l to current max
+
+{a,b,c}
+   l  r 
+abcabcbb
+
+{w,k}
+   lr
+ pwwkew
+
 '''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Frequency map and max length
-        freq_map = {}
+        l = 0
+        r = -1
+        seen = set()
         max_length = 0
         
-        # Pointers for sliding window
-        start = 0
-        end = 0
-        
-        # Loop through all characters in s
-        while end < len(s):
-            # Increase current chars count
-            char = s[end]
+        while r < len(s) - 1:
+            r += 1
+            start = s[l]
+            end = s[r]
             
-            # Set to 1 otherwise increase it's value
-            freq_map[char] = freq_map.get(char, 0) + 1
-            
-            # While char count is above 1 increment start and reduce removed char's count in map
-            while freq_map[char] > 1:
-                freq_map[s[start]] -= 1
-                start += 1
-                
-            # Valid window 
-            max_length = max(max_length, end + 1 - start)
-            
-            end += 1
-            
+            # Add chars that aren't duplicates
+            if end not in seen:
+                seen.add(end)
+                max_length = max(max_length, r-l + 1)
+            else:
+                # Move l pointer forward and remove chars
+                while s[l] != s[r]:
+                    seen.remove(s[l])
+                    l += 1
+                # Move l to one index past duplicate
+                l += 1
+                    
         return max_length
+                    
+        
