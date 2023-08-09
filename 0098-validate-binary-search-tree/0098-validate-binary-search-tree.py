29ee722de6c4ae25788239cd57913e222cf6f707
Camden C <49416039+balanced-energy@users.noreply.github.com>
2023-08-09 11:57:12 -0600

    Time: 45 ms (93.92%), Space: 18.7 MB (91.87%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,59 +1,39 @@
-'''
-Workflow Timestamps
-1. 0:00 - 1:15 Make Sure You Understand the Problem
-2. 1:15 - 5:20 Design a Solution / Runtime and Space Complexity
-3. 5:20 - 7:20 Write a Template for Code in Logical Blocks. Aka Pseudocode
-4. 7:20 - 11:44 Write the Code And Pass Test Cases.
-'''
-'''
-1. Make Sure You Understand the Problem
-2. Design a Solution / Runtime and Space Complexity
-Helper function that takes in min and max values. min and max start at negative and positive infinity
-if root is none return true
-These track the subtrees valid ranges when we call on left and right childred.
-if root not within range, return false. Continue to call function on children with root.val set as max when
-traversing to the left, and root.val set as min when traversing to the right. 
-return true
-
-3. Write a Template for Code in Logical Blocks. Aka Pseudocode
-# main
-    return _invalid_bst(root, -inf, inf)
-# helper
-_invalid_bst(root, min, max)
-    if root is none return true
-    
-    check root is within range
-    if not return false
-    
-    if not call on left with root as max 
-    if not call on right with root as min
-    
-    return true
-    
-4. Write the Code And Pass Test Cases.
-'''
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def _invalid_bst(root, min_val, max_val):
+
+'''
+Call helper function passing in -inf and inf as min, max values
+Create helper function with signature valid_bst(root, min, max)
+    Base case will be if root null 
+        Return True
+    Then check if root is within our given range
+        min <= root <= max
+        If not within range return False
+    Return call to helper from left child 
+        Left child means we need the new max to be our root
+        valid_bst(root.left, min, root)
+    AND 
+    Return call to helper from right child
+        Right child means we need new min to be our root
+        valid_bst(root.right, root, max)
+
+'''
+def valid_bst(root, minimum, maximum):
     if root is None:
         return True
     
-    if root.val <= min_val or root.val >= max_val:
+    if not minimum < root.val < maximum:
         return False
     
-    if not _invalid_bst(root.left, min_val, root.val):
-        return False
+    return valid_bst(root.left, minimum, root.val) and valid_bst(root.right, root.val, maximum)
     
-    if not _invalid_bst(root.right, root.val, max_val):
-        return False
     
-    return True
+    
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return _invalid_bst(root, float(-inf), float(inf))
+        return valid_bst(root, -inf, inf)
         
