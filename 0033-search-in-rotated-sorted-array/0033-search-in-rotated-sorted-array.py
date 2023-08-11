e3ce45141fe35c93e735d9394d485968f085fac5
Camden C <49416039+balanced-energy@users.noreply.github.com>
2023-08-11 09:45:35 -0600

    Time: 46 ms (83.50%), Space: 16.6 MB (93.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,58 +1,41 @@
+'''
+Run a modified binary search on the input array s
+    start = 0
+    end = len(s) - 1
+    While start <= end 
+        Calculate mid = (start + end) // 2
+        If s[mid] == target
+            Return mid
+        We need to check for two cases to find which half of the array is still sorted
+        Case 1: s[start] < s[mid] 
+            First half is still sorted 
+            If target greater than s[start] and less than s[mid].
+                We know the target is between start…mid indices.
+                Set end to mid - 1
+            Else 
+                Set start to mid + 1
+        Case 2: s[start] > s[mid]
+            Second half still sorted
+            If target greater than s[mid] and less than s[end].
+                We know the target is between mid..end indices.
+                Set start to mid + 1
+
+            Else 
+                Set end to mid - 1
+
+Return - 1. Target was not found
+
+[1], target = 0
+
+start = 0 
+end = 1 - 1 = 0
+
+mid = 0
+
+
 
 '''
-Workflow Timestamps
-1. 0:00 - 2:15 Make Sure You Understand the Problem
-2. 2:15 - 8:45 Design a Solution / Runtime and Space Complexity
-3. 8:45 - 14:45 Write a Template for Code in Logical Blocks. Aka Pseudocode
-4. Write the Code And Pass Test Cases.
-'''
-'''
-1. Make Sure You Understand the Problem
 
- l     m     r
-         l m r
-[4,5,6,7,0,1,2] target 0
-
- l     m     r
- l m r     
-[4,5,6,7,0,1,2] target 6
-
-2. Design a Solution / Runtime and Space Complexity
-Perform binary search. We want to compare mid to the start element to determine which half of the array is still sorted.
-Once we know which is still sorted we check if target is within that sorted half, if so move pointers to continue on that 
-half, otherwise move pointers to other half.
-
-3. Write a Template for Code in Logical Blocks. Aka Pseudocode
-start = 0
-end = length of array - 1
-
-while start <= end
-
-# Compare mid element to start 
-calculate mid 
-
-Check if mid == target 
-
-# First half still sorted
-If nums[mid] > nums[start]
-    if nums[start] <= target < nums[mid] 
-        right = mid - 1
-    else 
-        left = mid + 1
-# Second half still sorted
-if nums[mid] < nums[start]
-    if nums[mid] <= target < nums[start] 
-        left = mid + 1
-    else 
-        right = mid - 1
-        
-return - 1
-4. Write the Code And Pass Test Cases.
-
-[4,5,6,7,0,1,2] target 6
-
-[5,1,3] target 3
-'''
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start = 0
@@ -61,24 +44,23 @@ class Solution:
         while start <= end:
             mid = (start + end) // 2
             
+            # Check for found target
             if nums[mid] == target:
                 return mid
             
-            # First half is sorted
+            # Otherwise continue search, two cases
+            # First half still sorted
             if nums[start] <= nums[mid]:
                 if nums[start] <= target < nums[mid]:
                     end = mid - 1
-                    
                 else:
                     start = mid + 1
-                    
-            # Second half is sorted
-            else:
+            
+            # Second half is still sorted
+            elif nums[start] >= nums[mid]:
                 if nums[mid] < target <= nums[end]:
                     start = mid + 1
-                    
                 else:
                     end = mid - 1
-        
-        return - 1
-        
+            
+        return -1
