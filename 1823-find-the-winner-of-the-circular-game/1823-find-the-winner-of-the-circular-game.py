de60914ae3ea10d49a4f1ee8c9ee55287fc3d789
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-14 10:45:46 -0700

    Time: 19 ms (45.41%), Space: 11.7 MB (32.14%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def findTheWinner(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        friends = list(range(1,n+1))
+        i = 0
+        
+        while len(friends) > 1:
+            i = (i+k-1) % len(friends)
+            
+            friends.pop(i)
+        
+        return friends[0]
