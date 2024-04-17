2497886e622094fc77cd43b47d6434c044663347
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-17 06:48:33 -0700

    Time: 50 ms (30.10%), Space: 11.8 MB (13.91%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,65 +1,111 @@
+'''
+Understand the problem
+
+[9,9,9,9,9,9,9]
+[9,9,9,9]
+[8,9,9,9,0,0,0,1]
+
+[1,0,4,1]
+[2,3]
+
+
+[1]
+[9]
+[0,1]
+
+[8]
+[1]
+[9]
+
+[7]
+[5]
+[3,1]
+
+13 % 10 = 3
+13 % 10 = 1
+
+Design a solution 
+- Traverse through each node in the two lists while pointers aren't none 
+- summing node values tracking if there is a carry value
+- creating new node from sum
+- adding carry value to next iteration 
+
+    
+    initialize head node for lists_sum list
+    set carry = 0 
+    set curr pointer to head of lists_sum 
+    
+    while l1 or l2:
+        
+        check l1 and l2 values, if values set as val1 and val2
+        compute sum as val1 + val2 + carry 
+
+            
+        sum % 10 for node remainder
+        sum // 10 for carry 
+        
+        create new node from remainder 
+        set curr.next to remainder node    
+        set curr to curr.next in lists_sum
+        
+        check if l1, then move pointer
+        check if l2, then move pointer
+        
+        
+
+
+
+l1= [1,0,4,1]
+l2= [2,3]
+    [3,3,4,1]
+
+l1 = [8,1,4]
+l2 = [3,2]
+     [1,4,4]
+     
+     418 + 23 = 441
+     
+28:00 Test and code 
+
+'''
+
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-'''
-Workflow Timestamps
-1. 0:00 - 7:05 Make Sure You Understand the Problem
-2. 7:05 - 14:30 Design a Solution
-3. 14:30 - 38:30 Write a Template for Code in Logical Blocks. Aka Pseudocode
-4. 38:30 - 49:41 Write the Code And Pass Test Cases.
-'''
-'''
-1. Make Sure You Understand the Problem
-output = [8,9,9,9,0,0,0,1]
-Input: l1 = [9,9,9,9,9,9,9], 
-l2 =              [9,9,9,9]
-out = 9 + 9 = 18 % 10, carry 1, = [8]
-9 + 9 + carry = 19 carry 1 = [8, 9]
-9 + 9 + carry = 19 carry 1 = [8, 9, 9]
-9 + 9 + carry = 19 carry 1 = [8, 9, 9, 9]
-9 + carry (1) = 10 carry 1 = [8, 9, 9, 9, 0]
-9 + carry (1) = 10 carry 1 = [8, 9, 9, 9, 0, 0]
-9 + carry (1) = 10 carry 1 = [8, 9, 9, 9, 0, 0, 0]
-carry (1) = 1 =              [8, 9, 9, 9, 0, 0, 0, 1]
-
-
-test cases:
-1.)
-l1 = [9], l2 = [1] -> [0, 1]
-
-2.) 
-l1 = [1, 1] =, l2 = [9] -> [0, 2]
-
-
-2. Design a Solution
-Traverse lists. Check if both nodes exist, if only one, if none check for carry value append appropriate node value
-summing values through traversal, checking sum >= 10. Handle carry value, create new node append to output list
-
-3. Write a Template for Code in Logical Blocks. Aka Pseudocode
-
-4. Write the Code And Pass Test Cases.
-'''
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        #Initialize a new linked list 
-        dummy = ListNode(0)
-        cur = dummy
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        lists_sum = ListNode(None)
         carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0
+        curr = lists_sum
+        
+        while l1 or l2:
+            val1 = 0 if l1 is None else l1.val
+            val2 = 0 if l2 is None else l2.val
             
-            val_sum = l1Val + l2Val + carry
-            carry = val_sum // 10
+            nodes_sum = val1 + val2 + carry
+            remainder = nodes_sum % 10
+            carry = nodes_sum // 10
             
-            newNode = ListNode(val_sum % 10)
-            cur.next = newNode
-            cur = newNode
+            # Create new node and link
+            new_node = ListNode(remainder)
+            curr.next = new_node 
+            curr = curr.next 
             
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummy.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next 
             
+            if carry != 0:
+                curr.next = ListNode(carry)
+                
+            
+        return lists_sum.next 
