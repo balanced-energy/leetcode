48d2092e1bf1a612366c666bb4f1d712fe811900
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-17 09:57:44 -0700

    Time: 15 ms (70.98%), Space: 11.7 MB (45.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,62 +1,70 @@
 '''
 3:00 Understand the problem
-
 [1,2,3], n = 1
 [1,2]
-
-[1,2,3], n = 3
-[2,3]
 
 [1,2,3], n = 2
 [1,3]
 
-[1], n = 1
-[]
-
-fast = dummy, None
-slow = dummy, 
-
-17:45 Design a solution 
-
-- initialize dummy node and set dummy.next to head 
-- Traverse input list
-- Two pointers, fast and slow where fast is n + 1 ahead of slow
-- When fast is None set slow.next = slow.next.next 
-
-    initialize pointers to a dummy node
-    set dummy.next as head
-    move fast pointer n + 1 nodes ahead 
-    
-    while fast:
-        increment fast and slow pointers
-    
-    # Remove node
-    if slow.next:
-        slow.next = slow.next.next 
-    else:
-    return []
-    
-    return dummy.next 
-
-[1,2,3], n = 1
-fast=dummy,2,3,None
-slow=dummy,1,2
-[1,2]
-
 [1,2,3], n = 3
-fast = dummy,None
-slow = dummy,
 [2,3]
 
 [1], n = 1
-fast = dummy, None
-slow = dummy, 
 []
 
-d[1,2,3], n = 2
-fast = dummy,3,None 
-slow = dummy,1 
-[1,3]
+11:30 Design a solution 
+- initialize dummy node and set pointers to dummy
+- link dummy to input list by setting dummy.next 
+- Move the fast pointer n + 1 nodes 
+- Traverse linked list until the end where fast is None
+- remove node by setting slow.next = slow.next.next
+
+d[1,2,3], n = 1
+[1,2]
+fast = dummy,2,3,None 
+slow = dummy,1,2
+
+
+d[1,2,3], n = 3
+[2,3]
+fast = dummy, None
+slow = dummy 
+
+d[1], n = 1
+[]
+fast = dummy,None 
+slow = dummy
+
+
+initialize dummy node
+    set dummy next to input list head
+    set pointers to dummy
+    
+    move fast pointer n + 1 nodes
+    
+    while fast
+        incremenent fast and slow pointers
+    
+    set slow.next = slow.next.next
+    
+    return dummy.next 
+    
+Runtime: O(N)
+Space: O(1)
+
+
+Implement
+
+d[1], n = 1
+d->None
+fast = dummy, None
+slow = dummy, 
+
+d[1,2,3], n = 1
+[1,2]
+
+fast = dummy,2,3,None 
+slow = dummy,1,2
 '''
 
 # Definition for singly-linked list.
@@ -77,18 +85,15 @@ class Solution(object):
         fast = dummy
         slow = dummy
         
-        # Move fast pointer n + 1 nodes 
+        # Move fast n + 1 nodes
         for _ in range(0, n + 1):
             fast = fast.next 
-        
+            
         while fast:
             fast = fast.next 
-            slow = slow.next    
+            slow = slow.next 
+            
+        # Remove node
+        slow.next = slow.next.next
         
-        # Remove node 
-        if slow.next:
-            slow.next = slow.next.next
-        else:
-            return []
-    
-        return dummy.next
+        return dummy.next 
