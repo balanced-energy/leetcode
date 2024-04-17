bc72141a7dbad1874857af977097ace7dc2394e3
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-17 09:31:13 -0700

    Time: 19 ms (38.44%), Space: 11.7 MB (15.25%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,47 +1,94 @@
+'''
+3:00 Understand the problem
+
+[1,2,3], n = 1
+[1,2]
+
+[1,2,3], n = 3
+[2,3]
+
+[1,2,3], n = 2
+[1,3]
+
+[1], n = 1
+[]
+
+fast = dummy, None
+slow = dummy, 
+
+17:45 Design a solution 
+
+- initialize dummy node and set dummy.next to head 
+- Traverse input list
+- Two pointers, fast and slow where fast is n + 1 ahead of slow
+- When fast is None set slow.next = slow.next.next 
+
+    initialize pointers to a dummy node
+    set dummy.next as head
+    move fast pointer n + 1 nodes ahead 
+    
+    while fast:
+        increment fast and slow pointers
+    
+    # Remove node
+    if slow.next:
+        slow.next = slow.next.next 
+    else:
+    return []
+    
+    return dummy.next 
+
+[1,2,3], n = 1
+fast=dummy,2,3,None
+slow=dummy,1,2
+[1,2]
+
+[1,2,3], n = 3
+fast = dummy,None
+slow = dummy,
+[2,3]
+
+[1], n = 1
+fast = dummy, None
+slow = dummy, 
+[]
+
+d[1,2,3], n = 2
+fast = dummy,3,None 
+slow = dummy,1 
+[1,3]
+'''
+
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-'''
-Two pointers
-Start both at a dummy node
-Move first to n + 1 nodes ahead
-Then once end pointer hits null 
-first pointer is at node previous to the node to delete
-
-n = 2
-length = 5
-move to 5 - 2 node = 3
-remove
-     p
-[1,2,3,4,5]
-
-n = 1
-length = 1 
-move to 1 - 1 = 0
-remove  
-[1]
-'''
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        dummy = ListNode(None)
         dummy.next = head
-        
-        slow = dummy
         fast = dummy
+        slow = dummy
         
-        for i in range(n + 1):
-            fast = fast.next
+        # Move fast pointer n + 1 nodes 
+        for _ in range(0, n + 1):
+            fast = fast.next 
         
-        # Move each pointer forward until fast == None
-        while fast != None:
-            slow = slow.next
-            fast = fast.next
+        while fast:
+            fast = fast.next 
+            slow = slow.next    
         
-        # Remove node following slow pointer
-        slow.next = slow.next.next
-        
+        # Remove node 
+        if slow.next:
+            slow.next = slow.next.next
+        else:
+            return []
+    
         return dummy.next
-        
