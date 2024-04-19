04487b8b3abf6e6e99c27feddf4a112880703713
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-19 11:52:49 -0700

    Time: 73 ms (12.50%), Space: 29.4 MB (78.46%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,104 +1,128 @@
 '''
-Mock - 45m
-Workflow Timestamps
-1. Make Sure You Understand the Problem
-2. Design a Solution / Template / Runtime and Space Complexity
-3. Write the Code And Pass Test Cases.
-
-1. Make Sure You Understand the Problem
+3:40 Understand the problem
 [1] -> [1]
 
- f   e
-[1,3,2]
-  f   e
-[(1),(2),(3)]
+1,2 -> 1,2
 
+1,2,3 -> 1,3,2
 
+1,2,3,4,5,6 -> 1,6,2,5,3,4
 
-Even number of nodes
- f         e
-[1,2,3,4,5,6] -> [1,6,2,5,3,4]
-         f 
-[1,6,2,5,3,4]
-          f   e
-[(1),(2),(3),(4),(5),(6)]
+1,2,3,4,5,6,7 -> 1,7,2,6,3,5,4
 
- f          
+Design a solution
+- Find the middle node
+- Reverse from middle to end
+- Merge lists
+
+initialize fast, slow to head
+- while fast and fast.next 
+    - fast + 2
+    - slow + 1
+    
+set prev None
+cur = slow
+
+- while cur
+    - next = cur.next
+    - cur.next = prev
+    - prev = cur
+    - cur = next
+
+# Merge Lists
+head, tail = head, prev
+- while tail.next 
+    - tmp_head = head.next
+    - head.next = tail
+    - head = tmp_head
+    
+    - tmp_tail = tail.next
+    - tail.next = tmp_head
+    - tail = tmp_tail
+     
+   
+    
+
 [1,2,3,4,5]
+s=1,2,3
+f=1,3,5
 
-Get current end node
-decrement end pointer
-Detach end node in linked list
-Insert after front pointer node in linked list
-increment front in nodes aux list 
-loop ends when front.next = end pointer
+None<-3<-4<-5
+p=None
+c=3
 
-Odd number of nodes
-[1,2,3,4,5] -> [1,5,2,4,3]
-     f
-[1,5,2,4,3]
-          f               e
-nodes = [(1),(2),(3),(4),(5)]
-Get current end node
-decrement end pointer
-Detach end node in linked list
-Insert after front pointer node in linked list
-increment front in nodes aux list 
-loop ends when front == end pointer 
-
-end when front.next == end or front == end 
+[1,2,3,4,5]
+1->5->2
+h=1,2
+tmp_head=2
+t=5,
+tmp_tail=4
 
 
-2. Design a Solution / Template / Runtime and Space Complexity
- - Traverse linked list create aux nodes list with pointers to each node
-    - set front and end pointers 
- - while front.next != end pointer and front != end ponter in aux nodes list 
-    - get current end node
-    - decrement end pointer in aux nodes list
-    - insert end node after front pointer in linked list
-    - increment front node pointer in aux nodes list
+1,2,3 -> 1,3,2
+s=1,2
+f=1,3
 
-- return head 
+1->2    None<-2<-3
 
-[1,34,2,]
-3. Write the Code And Pass Test Cases.
+1->3->2
+h=1
+t_h = 2
+t = 3,2
+t_t = 2
+
+1,2 -> 1,2
+s=1,2
+f=1,None
+
+
+1,2->None
+prev = None,2
+cur = 2, None
+
+
+1,2
+1->2
+h=1
+h_n = 2
+
+t=2
+t_n = None
 '''
+
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+class Solution(object):
+    def reorderList(self, head):
         """
-        Do not return anything, modify head in-place instead.
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
         """
-        # Create aux nodes list with pointers to each node, set front, end pointers
-        # Initialize aux nodes list and set front pointer
-        nodes = []
-        cur = head
+        fast, slow = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        prev, cur = None, slow
+        
         while cur:
-            nodes.append(cur)
-            cur = cur.next 
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
             
-        front = 0
-        end = len(nodes) - 1
-        
-        while front != end and front + 1 != end:
-            # Get end node and decrement 
-            end_node = nodes[end]
-            end  -= 1
-            
-            # Detach removed node 
-            nodes[end].next = None
-            
-            # Insert after front node pointer
-            front_node = nodes[front]
-            end_node.next = front_node.next
-            front_node.next = end_node
-            front += 1
-            
-        return head
-            
+        tail = prev 
         
         
+        while tail.next:
+            tmp_head = head.next
+            head.next = tail
+            head = tmp_head
+            
+            tmp_tail = tail.next
+            tail.next = tmp_head
+            tail = tmp_tail
