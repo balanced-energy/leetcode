59cd643657d61a795c63bf45fd763eda4cadec46
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-22 12:58:55 -0700

    Time: 38 ms (53.82%), Space: 17.1 MB (92.25%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -1,3 +1,70 @@
+'''
+6:30 Understand the problem
+The nodes in the list must have all of their child pointers set to null.
+
+[] -> []
+
+[1] -> [1]
+
+1 -> [1,3]
+| 
+3
+
+
+1,2 -> [1,3,5,6,4,2]
+|
+3,4
+|
+5
+|
+6
+
+stack []
+d[1,3,2]
+prev = d,1,3
+curr= 1,3,2
+
+
+s[]
+d[1<>2<>3<>7<>8<>11<>12<>9<>10<>4<>5<>6]
+
+curr=1,2,3,7,8,11,12,9,10,4,5,6
+prev=d,1,2,3,7,8,11,12,9,10,4,5,6
+
+
+
+Design a solution
+DFS
+
+Use a stack, pushing first node on stack
+
+if not head 
+    return None
+    
+prev = dummy node
+
+while stack 
+    - point curr.prev to prev
+    - set prev.next to curr
+    
+    - if curr.next
+        append cur.next to stack
+    
+    - if curr has child
+        append child to stack
+        curr.child = None
+         
+    prev = curr
+    
+return dummy.next 
+
+
+Runtime: O(N)
+Space: O(N)
+
+32:00 Implement 
+'''
+
 """
 # Definition for a Node.
 class Node:
@@ -8,42 +75,33 @@ class Node:
         self.child = child
 """
 
-'''
-1-None
-|
-2-None
-'''
-
-def _flatten(head):
-    
-    cur = head
-    while cur:
-        next_node = cur.next 
-        if not next_node:
-            tail = cur
-            
-        if cur.child:
-            cur.next = cur.child
-            cur.child.prev = cur
-            
-            child_tail = _flatten(cur.child)
-            
-            if next_node:
-                next_node.prev = child_tail
-            
-            child_tail.next = next_node 
-            
-            cur.child = None
-            
-        cur = cur.next
-        
-    return tail
-    
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-    
-        _flatten(head)
-     
-        return head
+        
+        stack = []
+        stack.append(head)
+        
+        dummy = Node('dummy', None, None, None)
+        prev = dummy
+        
+        
+        while stack:
+            curr = stack.pop()
+            
+            curr.prev = prev
+            prev.next = curr
+            
+            if curr.next:
+                stack.append(curr.next)
+                
+            if curr.child:
+                stack.append(curr.child)
+                curr.child = None
+                
+            prev = curr
+        
+        dummy.next.prev = None
+        
+        return dummy.next 
