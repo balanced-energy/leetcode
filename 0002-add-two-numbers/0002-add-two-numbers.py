f8efc9ecfa79afb86ea6ed3d22627646fb840816
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-25 15:53:22 -0600

    Time: 66 ms (7.00%), Space: 16.7 MB (48.60%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,109 +1,86 @@
 '''
 Understand the problem
+                     1
+l1 = [9,9,9,9,9,9,9], 
+l2 = [9,9,9,9]
+res: [8,9,9,9,0,0,0,1]
 
-[9,9,9,9,9,9,9]
-[9,9,9,9]
-[8,9,9,9,0,0,0,1]
 
-
-[9]
+[1,2,3]
 [1]
-[0,1]
+[2,2,3]
 
-9 + 1 = 10
-
-[3,1]
-[2,4]
-[5,5]
-
-13 + 42 = 55
-
-
-[8,2,5]
-[4,2,1]
-[2,5,6]
-
-528 + 124 = 652
-
-Design a solution 
-
-- Traverse through the input linked lists
-- summing values of nodes if not none
-- find remainder using % 
-- handling the carry when sum > 10 
-- create new node in return list from remainder value
-- move curr pointer and list pointers to next node
-- handle last node if carry still exists 
-
-    initliaze lists_sum
-    set curr = lists_sum
-    set carry = 0
-    
-    while l1 is not None and l2 is not None or carry != 0:
-        set v1 and v2 values if not none
-        sum v1 + v2 + carry
-        get remainder with % 10
-        get carry with // 10
-        
-        create remainder node
-        set curr.next to remainder node
-        
-        move curr and l1 and l2 pointers
-    
-    return lists_sum.next 
-    
-
-l1 = [8,2,5]
-l2 = [4,2,1]
-     [2,5,6]
-     
-     528 + 124 = 652
-     
-[9]
 [1]
-[0,1]
+[1,2,3]
+[2,2,3]
 
-[1,8]
-[0]
-[1,8]
+   1 1 1
+[9,9,9] -> 999
+[1,1,1] -> 111
+[0,1,1,1] -> 1110
 
-n = length of l1
-m = length of l2
-Runtime: O(max(n,m))
-Space: O(max(n,m)+1)
+     1
+[9,9]
+[1]
+d>0,0>1
+Design a solution
+- carry variable init to 0, 
+- dummy node to return resulting sum list
+- curr pointer to build resulting sum list init to dummy
+
+- Traverse l1 and l2 nodes until l1 != None or l2 != None or carry != 0
+- check if l1 and l2 nodes, if nodes set l1_val and l2_val else make them 0
+- nodes_sum will be set to l1_val + l2_val + carry
+- carry = nodes_ sum // 10 
+- Create new node with value nodes_sum % 10
+- Set curr.next to the new node
+- Move curr to new node
+- check if l1.next and l2.next and move pointers, else set them to None
+
+- return dummy.next 
+
+
+l1 =  [2,4,3], 
+l2 =  [5,6,4]
+res= d[]
+
+carry = 0,1,0
+l1 = 2,4,3 
+l2 = 5,6,4
+curr = dummy,7,0,8
+dummy = dummy>7>0>8
+
+m = len(l1)
+n = len(l2)
+
+Runtime: O(max(m, n))
+Space: O(1)
 '''
 
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
-        lists_sum = ListNode(None)
-        curr = lists_sum
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
+        dummy = ListNode('Dummy')
+        curr = dummy 
         
-        while l1 is not None or l2 is not None or carry != 0:
-            v1 = 0 if l1 is None else l1.val
-            v2 = 0 if l2 is None else l2.val
-            nodes_sum = v1 + v2 + carry
+        while l1 != None or l2 != None or carry != 0:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
             
-            remainder = nodes_sum % 10
+            nodes_sum = l1_val + l2_val + carry
             carry = nodes_sum // 10
             
-            curr.next = ListNode(remainder)
-            curr = curr.next 
+            new_node = ListNode(nodes_sum % 10)
+            curr.next = new_node 
+            curr = new_node
             
-            if l1 is not None:
-                l1 = l1.next 
-            if l2 is not None:
-                l2 = l2.next 
-        
-        return lists_sum.next 
+            # Move pointers
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next 
