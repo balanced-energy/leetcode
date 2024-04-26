8fb51b657f703683881378417ee206db44851c17
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-26 09:03:55 -0600

    Time: 37 ms (76.07%), Space: 16.6 MB (68.43%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -1,137 +1,71 @@
 '''
-3:15 Understand the problem 
+3:40 Understand the problem
 
-d[]
-d[]
-prev = d
-head = None
-head.next = None 
+d[] -> []
 
-  p h
-d[1]
-d[1]
-prev = d
-head = 1
-head.next = None
+d[1,1,2] -> [2]
 
   p     h
-d[1,1,2]
-d[2]
-prev = d
-head = 1,1
-head.next = 1,2 
+d[1,2,2] -> [1]
 
-  p   h 
-d[1,2,2]
-d[1]
-prev = d
-head = 1,2
-head.next = 2,2 
+d[1,2,3,3,4,4,5] -> [1,2,5]
 
-p   h
-d[1,1,2,3,3]
-  p   h
-d[2,3,3]
-d[2]
-prev = d
-head = 1,2,3
-head.next = 1,3,3 
+p       h
+d[1,1,1] -> []
 
-[1,1,1,2,3]
-[2,3]
+[1,1,2,3,3] -> [2]
 
-[1,2,3,3,3]
-[1,2]
+19:50 Design a solution
 
-p     h
-d[1,1,1,2,3,3,3]
-p         h
-d[2,3,3,3]
-p
-d[2]
-prev = d,
-head = 1,1,1,2,3
-head.next = 1,1,2,3,3 
+- Init a dummy node, and point to head to help in the case where duplicates sublist is at the head
+- prev pointer set to dummy to handle deleting sublist when traversing input list
+- While we have a head pointer
+    # Identify the start of a duplicates sublist by checking 
+    - if head.next and head.val == head.next.val
+        # move head pointer to end of that list
+        - while head.next and head.val == head.next.val move head pointer
+        # head is pointing to end of sublist, delete sublist
+        - set prev.next = head.next 
+        
+    - else if we didn't find start of sublist, increment prev pointer
+    - increment head pointer
 
-Design a solution
-- initialize dummy node and point it to head
-- Traverse linked list checking head.val and head.next.val
-- if values are equal, then move head pointer to end of duplicates sublist 
-    - prev.next to head.next
-- else
-    - increment prev
-- increment head
-
-
-psedocode:
-    - initialize dummy set next to head
-    - prev = dummy
-    
-    while head:
-        if head.next and head.val == head.next.val:
-            while head.next and head.val == head.next.val 
-                - increment head pointer
-            
-            # Delete sublist
-            prev.next = head.next
-        else
-            prev = prev.next
-            
-        # increment head
-        head = head.next
-
-    return dummy.next 
-    
-
-return dummy.next 
+- return dummy.next 
 
 Runtime: O(N)
-Space: (1)
-25:30 Implement code
-p     h
-d[1,1,1,2,3]
-p     h  
-d[2,3]
-prev = d
-head = 1
-head.next = 1
+Space: O(1)
 
-p   h  
-d[1,1,2,3,3]
-  p h
-d[2]
-prev = d,2
-head = 1,2
-head.next = 1,3
+p   h
+d[1,1,2] -> [2]
+
+Implement
+
+    p       h
+d[1,1,1,2,3]
 '''
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def deleteDuplicates(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        
-        dummy = ListNode(None, head)
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode('Dummy', head)
         prev = dummy
         
         while head:
             if head.next and head.val == head.next.val:
+                # Move head pointer to end of duplicates sublist
                 while head.next and head.val == head.next.val:
-                    head = head.next
-            
-                # Delete sublist of duplicates
-                prev.next = head.next
-            
-            else:
-                prev = prev.next
-            
-            # Increament head
+                    head = head.next 
+                    
+                # Delete duplicates sublist
+                prev.next = head.next 
+                
+            else: 
+                prev = prev.next 
+                
             head = head.next 
             
-        return dummy.next
-    
+        return dummy.next 
+                    
