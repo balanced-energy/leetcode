5093ca7865dd885164551e969d5473be0b8a29e1
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-04-30 16:31:06 -0600

    Time: 27 ms (98.05%), Space: 17.6 MB (8.12%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,83 +1,82 @@
 '''
-4:15 Understand the problem
+6:00 Understand the problem
 [] -> []
 
-[[1,0]] -> [[1,0]]
+[[1,None]] 
 
-[[1,null]] -> [[1,null]]
+[[1, 0]]
 
-[[1,1],[2,1]] -> [[1,1],[2,1]]
-
-[[3,null],[3,0],[3,null]] -> [[3,null],[3,0],[3,null]]
-
-[[1,2],[2,0],[3,1]] -> [[1,2],[2,0],[3,1]]
-
-
-29:00 Design a solution
-
-- visited map old_node:new_node  
-- recursive function input of head
-    - if head is None: return None
-    - if node in visited
-        return visited[head]
-     
-    - create new node
-    - add it to visited 
-    - new_node.next = func(head.next)
-    - new_node.random = func(head.random)
-    
-    return node
-
-{old_node(1):new_node(1), 2:2}
+[[1,1],[2,1]] 
+{(1),(2)}
 [[1,1],[2,1]]
 
-{1:1}
-[[1,0]] -> [[1,0]]
+              h
+[[1,2],[2,0],[3,None]]
+{(1),(2),(3)}
+[[1,2]>[2,0]>[3,None]>None]
 
-{7:7, 13:13, 11:11, 10:10, 1:1}
-     
+
+[[1,None], [2,None],[3,None]]
+
+
+  
 [[7,null],[13,0],[11,4],[10,2],[1,0]]
 
-{1:1,2:2,3:3 }
-[[1,2],[2,0],[3,1]] 
 
+28:00 Design a solution
+- We want to create a new node based on current nodes value in input list. 
+- Set next pointer
+- Set random pointer 
+
+- Using a set track nodes that are created or not
+
+    - Setting pointers could lead to pointing to a node that isn't created yet 
+
+
+recursive function 
+    - base case when node is None
+    - if node is already in set, return node 
+    
+    - create the node with curr nodes value
+    - add to created set
+    
+    -  new_node.next = recursive_cal(head.next)
+    -  new_node.random = recursive_cal(head.random)
+    
+    -  return new_node
 
 Runtime: O(N)
 Space: O(N)
 '''
+
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x, next=None, random=None):
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
 
-class Solution(object):
-    def copyRandomList(self, head):
-        """
-        :type head: Node
-        :rtype: Node
-        """
-        
+class Solution:
+    def __init__(self):
         self.visited = {}
-        return self.copy_node(head)
-        
-    def copy_node(self, head):
+    
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head == None:
             return None
-
+        
+        # Check set for head 
         if head in self.visited:
             return self.visited[head]
-
-        node = Node(head.val, None, None)
-        self.visited[head] = node 
-
-        # Recursively call on head.next and head.random
-        node.next = self.copy_node(head.next)
-        node.random = self.copy_node(head.random)
-
-        return node
-
+        
+        # Create new node with head value and add to set
+        new_node = Node(head.val)
+        
+        self.visited[head] = new_node
+        
+        new_node.next = self.copyRandomList(head.next)
+        new_node.random = self.copyRandomList(head.random)
+        
+        return new_node 
         
