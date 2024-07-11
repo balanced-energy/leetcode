0576b46d10a493bc6cd9d78e5f9251eca1dccbd8
Camden C <49416039+balanced-energy@users.noreply.github.com>
2024-07-11 09:31:46 -0600

    Time: 36 ms (48.78%), Space: 16.6 MB (50.31%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,24 +1,7 @@
-'''
-Starting from end identify first character and then sequence of characters until space is found
-
-while last character is space, decrement pointer
-
-# now on a character
-increment count until space or beginning of s
-'''
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        # Check last character for space
-        end = len(s) - 1
-        last_word_count = 0
-        
-        # Increment pointer to first char
-        while s[end] == ' ' and end >= 0:
-            end -= 1
-        
-        # Cound length of string until space
-        while s[end] != ' ' and end >= 0:
-            last_word_count += 1
-            end -= 1
-            
-        return last_word_count
+         # Split the string by spaces
+        words = s.split()
+
+        # Return the length of the last word
+        return len(words[-1])
